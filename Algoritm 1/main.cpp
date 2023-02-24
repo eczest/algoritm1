aad34779f7b460721db5efef9e268137ce8a5ba8
eczest <s-lakiza@bk.ru>
2023-02-24 23:20:31 +0500

    top

--- Algoritm 1/main.cpp
+++ Algoritm 1/main.cpp
@@ -1,15 +1,31 @@
 #include "Function.h"
-#include <iostream>
 #include <string>
 #include<vector>
 #include <stdio.h>
+#include <cstdio>
+#include <time.h>
+#include <fstream>
+#include <ctime>
+#include <iterator>
 using namespace std;
 int main (){
-    const int a =5;
+    int a,b;
+    cout<<"array lenght";
+    cin>>a;
+    cout<<"number range";
+    cin>>b;
+    int array[a];
+    for(int i=0;i<a;++i){
+        array[i]=rand()%b;
+    }
+    sum_array(array,a);
+    multi_array(array,a);
+    sort_puziri(array,a);
+    /*const int a =5;
     int b;
     cout<<"number range :";
     cin>>b;
     int array[a];
     //sort_puziri(array,a);
-     vvodfile(array,a);
+     vvodfile(array,a);*/
 }
